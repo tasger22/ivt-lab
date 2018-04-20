5171d15702c74a095463d669ed1164425a7c41d2
Gergo <tasger22@gmail.com>
2018-04-20 04:03:47 -0700

    Fixed firing issue

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,7 +79,12 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
+          firingSuccess = primaryTorpedoStore.fire(1);
+          firingSuccess = secondaryTorpedoStore.fire(1);
 
+          wasPrimaryFiredLast = true;
+        }
         break;
     }
 
